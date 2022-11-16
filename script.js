a025f742282862cd86f70691a6311b0a463cabec
Nikola Krstevski <36802068+nikolaesp@users.noreply.github.com>
2022-11-16 14:54:28 +0100

    prova 1

---
+++ script.js
@@ -0,0 +1,27 @@
+//creamo una variabile stringa e prede valore di utente
+let parolains = prompt("Inserisci parola:");
+let isPal = epalindrom(parolains); //chiama funzione
+console.log(isPal);
+
+
+
+
+
+
+
+
+
+
+
+
+// funzione che verifica se una parola e parindroma o no
+function epalindrom(parola){
+    let n = parola.lenght;
+    for (let i = 0; i < len / 2; i++) {  
+        // validate the first and last characters are the same  
+        if (str[i] !== str[len - 1 - i]) {  
+          return false;
+        } 
+    }  
+    return true;
+}
